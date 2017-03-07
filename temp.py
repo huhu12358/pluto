35688ed226fd73f028148fe3e006cc2f7c66bd72
mengwen_hu <humengwen668@126.com>
2017-03-07 16:44:58 +0800

    添加因子计算实例：Kurtosis

--- temp.py
+++ temp.py
@@ -19,6 +19,13 @@ sql = """SELECT S_INFO_WINDCODE sid, TRADE_DT trade_dt, S_DQ_CLOSE close
 data = pd.read_sql(sql, engine)
 
 # todo factor calculation
+"""
+对交易日排序
+计算单只股票收益率
+rolling计算20日交易方差（日度方差）
+转化为年度方差
+去掉多余的列
+"""
 window = 20
 example = '603998.SH'
 data1 = data[data['sid']==example].sort_values(by='trade_dt')
@@ -26,3 +33,24 @@ data2 = pd.concat([data1[['sid','trade_dt']], data1['close'].pct_change().rename
 data3 = data2.rolling(window,1).var().rename(columns={'return':'ret_variance'})
 data3['y_ret_var'] = data3['ret_variance']*sqrt(250)
 data3.drop('ret_variance',axis = 1,inplace=True)
+
+
+# factors: kurtosis20, like kurtosis60, kurtosis120
+# todo select data
+sql = """SELECT S_INFO_WINDCODE sid, TRADE_DT trade_dt, S_DQ_CLOSE close
+            FROM mercury.ashare_eod_prices WHERE TRADE_DT > {0}""".format('20160101')
+
+data = pd.read_sql(sql, engine)
+
+# todo factor calculation
+"""
+对交易日排序
+计算单只股票收益率
+rolling计算20日峰度
+去掉多余的列
+"""
+window = 20
+example = '603998.SH'
+data1 = data[data['sid']==example].sort_values(by='trade_dt')
+data2 = pd.concat([data1[['sid','trade_dt']], data1['close'].pct_change().rename('return')],axis = 1)
+data3 = data2.rolling(window,1).kurt().rename(columns={'return':'ret_kurtosis'})
