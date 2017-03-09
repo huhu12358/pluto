4b85dfcff3c7aa0a58fb77869b52552a39bbf539
mengwen_hu <humengwen668@126.com>
2017-03-09 10:05:34 +0800

    treynor20 not completed

--- temp.py
+++ temp.py
@@ -154,7 +154,7 @@ print(data5)
 
 
 
-# factors: sharperatio20, sharperatio60, sharperatio120
+# factors: sharperatio20, like sharperatio60, sharperatio120
 # todo select data
 date = '20170101'
 benchmark = 'CGB1Y.WI'
@@ -185,6 +185,65 @@ data2_riskfree  = pd.concat([data1_riskfree[['sid','trade_dt']], data1_riskfree[
 data3 = data2.join(data2_riskfree.set_index('trade_dt'), on='trade_dt', lsuffix='_s', rsuffix='_rf')
 
 # rolling计算：sharpe = (E(r_s) - E(r_f))/std_s，对式中的每一项rolling计算
+Er_s = (1.0+data3['return_s'].rolling(window=window).mean())**250-1.0
+Er_f = data3['return_rf']
+std_s = data3['return_s'].rolling(window=window).std()
+sharpe = (Er_s - Er_f)/std_s
+
+data4 = pd.concat([data4[['sid_s','trade_dt']],sharpe],axis=1).rename(columns = {0:'sharperatio'})
+data4.rename(columns={'sid_s':'sid'},inplace=True)
+
+print(data5)
+
+
+# factors: treynor20, like
+# todo select data
+date = '20170101'
+index = '000906.SH'
+benchmark = 'CGB1Y.WI'
+sql = """SELECT S_INFO_WINDCODE sid, TRADE_DT trade_dt, S_DQ_CLOSE close_s
+            FROM mercury.ashare_eod_prices WHERE TRADE_DT > '{0}'""".format(date)
+data = pd.read_sql(sql, engine)
+sql_index = """SELECT S_INFO_WINDCODE sid, TRADE_DT trade_dt, S_DQ_CLOSE close_i
+                        FROM mercury.aindex_eod_prices
+                        WHERE TRADE_DT > '{0}' AND S_INFO_WINDCODE = '{1}'""".format(self.date_back_to, index)
+data_index = pd.read_sql(sql_index, engine)
+sql_riskfree = """SELECT S_INFO_WINDCODE sid, TRADE_DT trade_dt, S_DQ_CLOSE close_rf
+                FROM wind.CGBBENCHMARK
+                WHERE TRADE_DT > '{0}' AND S_INFO_WINDCODE = '{1}'""".format(date, benchmark)
+data_riskfree = pd.read_sql(sql_riskfree, engine)
+
+# todo factor calculation, not complete
+"""
+对交易日排序
+计算单只股票日收益率，计算一年期国债每日年化收益率
+以股票时间为基准合并股票Dataframe与指数Dataframe
+rolling计算: sharpe = (E(r_s) - E(r_f))/std_s，对式中的每一项rolling计算
+去掉多余的列，标准化列名
+"""
+window = 20
+example = '603998.SH'
+data1           = data[data['sid']==example].sort_values(by='trade_dt')
+data1_index     = data_index.sort_values(by='trade_dt')
+data1_riskfree  = data_riskfree.sort_values(by='trade_dt')
+
+# 计算单只股票日收益率,计算指数日收益率，一年期国债年化收益率
+data2           = pd.concat([data1[['sid','trade_dt']], data1['close_s'].pct_change().rename('return')],axis = 1)
+data2_index     = pd.concat([data1_index[['sid','trade_dt']], data1_index['close_i'].pct_change().rename('return')],axis = 1)
+rf_return = (1+data1_riskfree['close_rf']/100)**(1.0/250)-1.0
+data2_riskfree  = pd.concat([data1_riskfree[['sid','trade_dt']], rf_return.rename('return')],axis = 1)
+
+# rolling计算: 股票与指数收益率协方差/单只股票收益率标准差 -> beta;
+data3 = data2.join(data2_index.set_index('trade_dt'),on='trade_dt',lsuffix='_s',rsuffix='_i')
+data3 = data3.join(data2_riskfree.set_index('trade_dt'),on='trade_dt')\
+    .rename(columns={'sid':'sid_rf','return':'return_rf'})
+cov = data3['return_s'].rolling(window=window).cov(other=data3['return_i'])
+std = data3[['return_s']].rolling(window=window).std().rename(columns={'return_s':'std_s'})
+beta = cov/std['std_s']
+
+# rolling计算：sharpe = (E(r_s) - E(r_f))/std_s，对式中的每一项rolling计算
+data4 = pd.concat([data3,beta],axis=1,ignore_index=False).rename(columns={0:'beta'})
+
 Er_s = (1.0+data3['return_s'].rolling(window=window).mean())**250-1.0
 Er_f = data3['return_rf']
 std_s = data3['return_s'].rolling(window=window).std()
