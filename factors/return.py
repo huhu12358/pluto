74f2392a9ce23e82f3292ed294512c8224731211
mengwen_hu <humengwen668@126.com>
2017-03-10 12:59:50 +0800

    添加了详细注释

--- factors/return.py
+++ factors/return.py
@@ -8,9 +8,45 @@ from factors_handler import FactorBase
 
 # todo write the documentation of factor calculation
 """
-因子描述：
-数据调用：
+数据调用：mercury.ashare_eod_prices, mercury.aindex_eod_prices, mercury.cgb_benchmark
+
+因子描述：VARIANCE20: 20 日收益方差。类似VARIANCE20，VARIANCE60，VARIANCE120
 计算方法：
+对当前日期之前的20个交易日（包括今日）的收益率计算方差，得到日度方差
+注：因子值为年化后的值，等于日度方差*sqrt(250)。
+
+
+因子描述：KURTOSIS20：收益的20日峰度。类似KURTOSIS20，KURTOSIS60，KURTOSIS120
+计算方法：对当前日期之前的20个交易日（包括今日）的收益率计算峰度
+
+
+因子描述：BETA20：20 日 beta 值，指数使用中证500指数
+            类似BETA20，BETA60，BETA120，BETA252
+计算方法：计算股票与指数收益率协方差与单只股票收益率标准差，相除得到beta值
+            beta = cov(r_s, r_i) / std(r_s)
+
+
+因子描述：ALPHA20：20 日 Jensen's alpha, 指数使用中证500指数，无风险利率使用一年期国债CGB1Y
+            类似ALPHA20，ALPHA60，ALPHA120
+计算方法：股票与指数收益率协方差/单只股票收益率标准差 -> beta;
+            Jensen's alpha = Portfolio Return − [Risk Free Rate + Portfolio Beta * (Market Return − Risk Free Rate)]
+            alpha = E(r_s) - [E(r_f) + beta_s_i * (E(r_i) - E(r_f))]， 均值取20日均值
+
+
+因子描述：SHARPERATIO20：20 日 SharpeRatio，无风险利率使用一年期国债CGB1Y。
+            类似SHARPERATIO20，SHARPERATIO60，SHARPERATIO120
+计算方法：sharpe = (E(r_s) - E(r_f))/std(r_s)
+
+
+因子描述：TREYNORRATIO20：20 日 TreynorRatio，无风险利率使用一年期国债CGB1Y，beta指数使用中证500指数
+            类似TREYNORRATIO20，TREYNORRATIO60，TREYNORRATIO120
+计算方法：(E(r_s) - E(r_f))/beta
+
+
+因子描述：INFORMATIONRATIO20：20 日 InformationRatio，指数使用中证500指数
+            类似INFORMATIONRATIO20，INFORMATIONRATIO60，INFORMATIONRATIO120
+计算方法：IR = E(r_s - r_i)/std(r_s - r_i)
+
 """
 
 
@@ -48,7 +84,7 @@ class RETURN(FactorBase):
                         WHERE TRADE_DT > '{0}' AND S_INFO_WINDCODE = '{1}'""".format(self.date_back_to, index)
         data_index = pd.read_sql(sql_index, engine)
         sql_riskfree = """SELECT S_INFO_WINDCODE sid, TRADE_DT trade_dt, S_DQ_CLOSE close
-                        FROM wind.CGBBENCHMARK
+                        FROM mercury.cgb_benchmark
                         WHERE TRADE_DT > '{0}' AND S_INFO_WINDCODE = '{1}'""".format(self.date_back_to, riskfree)
         data_riskfree = pd.read_sql(sql_riskfree, engine)
         # </editor-fold>
@@ -69,8 +105,7 @@ class RETURN(FactorBase):
         dict = {'close': data1, 'return': data2}
         panel = pd.Panel.from_dict(dict)
 
-
-        # 收益率的方差，滚动计算
+        """收益率的方差，滚动计算"""
         panel['ret_var' + str(self.window)] = panel['return'].rolling(self.window).var()
         if self.factor == 'VARIANCE':
             r = panel['ret_var' + str(self.window)].copy()
@@ -78,8 +113,7 @@ class RETURN(FactorBase):
             df = standard_data(r)
             return df
 
-
-        # 收益率的峰度，滚动计算
+        """收益率的峰度，滚动计算"""
         panel['ret_kurt' + str(self.window)] = panel['return'].rolling(self.window).kurt()
         # 截面去极值
         panel['ret_kurt' + str(self.window)][abs(panel['ret_kurt' + str(self.window)]) > 10e10] = np.nan
@@ -89,7 +123,7 @@ class RETURN(FactorBase):
             df = standard_data(r)
             return df
 
-        # 收益率beta，滚动计算
+        """收益率beta，滚动计算"""
         # 1）收益率标准差，滚动计算
         panel['ret_std' + str(self.window)] = panel['return'].rolling(self.window).std()
         # 2）各个股票对index的协方差，滚动计算
@@ -106,8 +140,7 @@ class RETURN(FactorBase):
             df = standard_data(r)
             return df
 
-
-        # 收益率alpha，滚动计算
+        """收益率alpha，滚动计算"""
         # alpha_J = E(r_s) - [E(r_f) + beta_s_i * (E(r_i) - E(r_f))]
         # E(r_s)
         panel['ret_mean' + str(self.window)] = panel['return'].rolling(self.window).mean()
@@ -136,9 +169,7 @@ class RETURN(FactorBase):
             df = standard_data(r)
             return df
 
-
-
-        # 收益率sharperatio，滚动计算
+        """收益率sharperatio，滚动计算"""
         # sharpe = (E(r_s) - E(r_f))/std_s
         def sub_temp(s):
             return s - temp
@@ -147,27 +178,43 @@ class RETURN(FactorBase):
             temp = riskfree_r_mean
             panel['ret_sharpe' + str(self.window)] = panel['ret_mean' + str(self.window)].apply(sub_temp, axis=0) \
                                                      / panel['ret_std' + str(self.window)]
+            # 截面去极值
             panel['ret_sharpe' + str(self.window)][abs(panel['ret_sharpe' + str(self.window)]) > 10e10] = np.nan
             r = panel['ret_sharpe' + str(self.window)].copy()
             r.drop(remove, axis=1, inplace=True)
             df = standard_data(r)
             return df
 
-
-        # 收益率TreynorRatio，滚动计算
+        """收益率TreynorRatio，滚动计算"""
         # treynor = (E(r_s) - E(r_f))/beta
         if self.factor == 'TREYNORRATIO':
             temp = riskfree_r_mean
             panel['ret_treynor' + str(self.window)] = panel['ret_mean' + str(self.window)].apply(sub_temp, axis=0) \
                                                       / panel['ret_beta' + str(self.window)]
+            # 截面去极值
             panel['ret_treynor' + str(self.window)][abs(panel['ret_treynor' + str(self.window)]) > 10e10] = np.nan
             r = panel['ret_treynor' + str(self.window)].copy()
             r.drop(remove, axis=1, inplace=True)
             df = standard_data(r)
             return df
 
+        """收益率InformationRatio，滚动计算"""
+        # IR = E(r_s - r_i)/std(r_s - r_i)
+        if self.factor == 'INFORMATIONRATIO':
+            temp = index_r
+            # r_s - r_i
+            panel['ret_temp1' + str(self.window)] = panel['return'].apply(sub_temp, axis=0)
+            # IR = E(r_s - r_i)/std(r_s - r_i)
+            panel['ret_ir' + str(self.window)] = panel['ret_temp1' + str(self.window)].rolling(self.window).mean() \
+                                                 - panel['ret_temp1' + str(self.window)].rolling(self.window).std()
+            # 截面去极值
+            panel['ret_ir' + str(self.window)][abs(panel['ret_ir' + str(self.window)]) > 10e10] = np.nan
 
-                #return df
+            r = panel['ret_ir' + str(self.window)].copy()
+            r.drop(remove, axis=1, inplace=True)
+            df = standard_data(r)
+            return df
+
 
 
 if __name__ == '__main__':
@@ -175,7 +222,7 @@ if __name__ == '__main__':
         "mysql+pymysql://hsquant:hs123456@218.1.122.196:3306/factors?charset=utf8&autocommit=true", echo=False
     )
     # todo write factor info
-    """
+    """"""
     # 方差因子
     factor_var20 = RETURN(engine, 'VARIANCE20', '20日收益方差', 14, 'VARIANCE',20)
     factor_var20.update()
@@ -240,10 +287,31 @@ if __name__ == '__main__':
 
 
 
-
-"""
-    # treynorratio
+    # treynorratio 因子
     factor_treynor20 = RETURN(engine, 'TREYNORRATIO20', '20 日 TreynorRatio，'\
                                                         '无风险利率使用一年期国债CGB1Y，'\
                                                         'beta指数使用中证500指数', 14, 'TREYNORRATIO', 20)
     factor_treynor20.update()
+
+    factor_treynor60 = RETURN(engine, 'TREYNORRATIO20', '60 日 TreynorRatio，'\
+                                                        '无风险利率使用一年期国债CGB1Y，'\
+                                                        'beta指数使用中证500指数', 14, 'TREYNORRATIO', 60)
+    factor_treynor60.update()
+
+    factor_treynor120 = RETURN(engine, 'TREYNORRATIO20', '120 日 TreynorRatio，'\
+                                                        '无风险利率使用一年期国债CGB1Y，'\
+                                                        'beta指数使用中证500指数', 14, 'TREYNORRATIO', 120)
+    factor_treynor120.update()
+
+    # informationratio, IR 因子
+    factor_IR20 = RETURN(engine, 'INFORMATIONRATIO20', '20 日 InformationRatio，' \
+                                                       '指数使用中证500指数', 14, 'INFORMATIONRATIO', 20)
+    factor_IR20.update()
+
+    factor_IR60 = RETURN(engine, 'INFORMATIONRATIO20', '60 日 InformationRatio，' \
+                                                       '指数使用中证500指数', 14, 'INFORMATIONRATIO', 60)
+    factor_IR60.update()
+
+    factor_IR120 = RETURN(engine, 'INFORMATIONRATIO20', '120 日 InformationRatio，' \
+                                                       '指数使用中证500指数', 14, 'INFORMATIONRATIO', 120)
+    factor_IR120.update()
